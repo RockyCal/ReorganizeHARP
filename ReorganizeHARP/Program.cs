596caeaacd2a87f154a91b5f3192949f116212f4
RockyCal <calderon.rocky@gmail.com>
2016-01-25 14:35:45 -0800

    added sshHostKeyFingerprint

--- ReorganizeHARP/Program.cs
+++ ReorganizeHARP/Program.cs
@@ -6,7 +6,7 @@ namespace ReorganizeHARP
 {
     class Program
     {
-        static void Main(string[] args)
+        public static int Main(string[] args)
         {
             try
             {
@@ -16,24 +16,28 @@ namespace ReorganizeHARP
                     Protocol = Protocol.Sftp,
                     HostName = ConfigurationManager.AppSettings["hostName"],
                     UserName = ConfigurationManager.AppSettings["userName"],
-                    Password = ConfigurationManager.AppSettings["password"]
+                    Password = ConfigurationManager.AppSettings["password"],
+                    SshHostKeyFingerprint = ConfigurationManager.AppSettings["sshHostKeyFingerprint"]
                 };
 
                 using (Session session = new Session())
                 {
+                    System.Diagnostics.Debug.Write("made it to here");
                     // Connect
                     session.Open(sessionOptions);
 
                     RemoteDirectoryInfo directory = session.ListDirectory("/home/cetus/shared/HARP^ Deployment^ and^ Recovery^ Files");
                     foreach (RemoteFileInfo fileInfo in directory.Files)
                     {
-                        Console.WriteLine("{0}", fileInfo.Name);
+                        System.Diagnostics.Debug.Write("{0}", fileInfo.Name);
                     }
                 }
+                return 0;
             }
             catch (Exception e)
             {
-                Console.WriteLine("Error: {0}", e);
+                System.Diagnostics.Debug.Write("Error: {0}", e.ToString());
+                return 1;
             }
         }
     }
